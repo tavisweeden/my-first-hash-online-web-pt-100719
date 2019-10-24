f6de73698c7cc3ab4d7bfeb521734e38067d6a9e
Tavis Weeden <tavisweeden@gmail.com>
2019-10-24 00:32:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,4 +24,9 @@ shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest
+  shipping_manifest = {                      
+                      "whale bone corsets" => 5, 
+                      "porcelain vases" => 2, 
+                      "oil paintings" => 3 }
+ shipping_manifest[]                     
+end                      
