daf384c873066768f815d5778512e8b7c3903f01
Tavis Weeden <tavisweeden@gmail.com>
2019-10-24 00:36:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def adding
   shipping_manifest = {                      
                       "whale bone corsets" => 5, 
                       "porcelain vases" => 2, 
-                      "oil paintings" => 3
+                      "oil paintings" => 3,
                       "muskets" => 2, "gun powder => 4"}
 
 end                      
