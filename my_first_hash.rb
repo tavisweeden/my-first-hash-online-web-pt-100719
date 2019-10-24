163d0f102341f7416d89e02f9cce04131e3123d3
Tavis Weeden <tavisweeden@gmail.com>
2019-10-24 00:38:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,6 +28,7 @@ def adding
                       "whale bone corsets" => 5, 
                       "porcelain vases" => 2, 
                       "oil paintings" => 3,
-                      "muskets" => 2, "gun powder => 4"}
+                      "muskets" => 2, 
+                      "gun powder" => 4}
 
 end                      
