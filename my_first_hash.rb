b3669db263267f8f2c75f3ffb150c9cbeb953764
Tavis Weeden <tavisweeden@gmail.com>
2019-10-24 00:30:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,7 +12,7 @@ def shipping_manifest
 
 end
 
-def retrieval(shipping_manifest)
+def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
@@ -20,7 +20,7 @@ def retrieval(shipping_manifest)
                  }
 
   #Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
-retrieval("oil paintings")
+
 
